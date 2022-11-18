04d9a2aec8e03c945b7af74913b3308c5e71e975
Sergen Türkmendağ <sergenturkmendag@gmail.com>
2022-11-18 10:18:01 +0200

    axios projesi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import React,{useState,useEffect} from "react";
 function App() {
+  const [veri, setVeri]=useState("");
+  const [id, setId]=useState(1);
+
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then(res=>setVeri(res.data[id].title))
+    .catch(err=>console.log(err))
+  },[veri, id])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> {veri} </h1> 
+     <input type="text" value={id} onChange={e=>setId(e.target.value)} />
     </div>
   );
 }
